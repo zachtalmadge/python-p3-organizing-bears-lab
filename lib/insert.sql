1b4f4875a74fbfdf4d23eddc398513c8349dd9af
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-26 22:17:07 -0400

    pass tests

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr.Chocolate', 5, 'M', 'brown', 'good', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 5, 'M', 'brown', 'good', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 5, 'M', 'brown', 'good', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 5, 'M', 'brown', 'good', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 5, 'M', 'brown', 'good', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 5, 'M', 'brown', 'good', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 5, 'M', 'brown', 'good', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (null, 5, 'M', 'brown', 'good', true);
